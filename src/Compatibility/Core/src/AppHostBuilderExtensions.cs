94bbd79f96afef02792a23f3e84272e94e6d3794
Kangho Hur <kangho.hur@samsung.com>
2021-08-26 14:30:07 +0900

    Fix build and runtime error after bumping to latest code

--- src/Compatibility/Core/src/AppHostBuilderExtensions.cs
+++ src/Compatibility/Core/src/AppHostBuilderExtensions.cs
@@ -154,6 +154,9 @@ namespace Microsoft.Maui.Controls.Hosting
 					handlers.TryAddCompatibilityRenderer(typeof(ViewCell), typeof(ViewCellRenderer));
 					handlers.TryAddCompatibilityRenderer(typeof(SwitchCell), typeof(SwitchCellRenderer));
 
+#if TIZEN
+					handlers.TryAddCompatibilityRenderer(typeof(ContentView), typeof(LayoutRenderer));
+#else
 					// This is for Layouts that currently don't work when assigned to LayoutHandler
 					handlers.TryAddCompatibilityRenderer(typeof(ContentView), typeof(DefaultRenderer));
 
